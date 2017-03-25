2dd9a19304c94ca4a32dca1da56eca0f203cb76e
Barloklol <markustibet@gmail.com>
2017-03-26 00:26:38 +0100

    Update utils.py

--- monocle/utils.py
+++ monocle/utils.py
@@ -141,7 +141,7 @@ def get_altitudes(coords):
             #r = requests.get('https://maps.googleapis.com/maps/api/elevation/json',
             #                 params=params).json()
 
-            params = {'locs': polyline.encode((point,))}
+            params = {'locs': polyline.encode(coords)}
             r = requests.get('http://alt.festzeit.ch/',
                              params=params).json()
 

abb9c64cd6f7da70f28b3736bacd879736f73929
Barloklol <markustibet@gmail.com>
2017-04-16 12:51:43 +0200

    Update worker.py

--- monocle/worker.py
+++ monocle/worker.py
@@ -795,8 +795,8 @@ class Worker:
                     if not self.normalize_gym(fort) in FORT_CACHE:
                         request = self.api.create_request()
                         request.get_gym_details(gym_id=fort['id'],
-                                                player_latitude=latitude,
-                                                player_longitude=longitude,
+                                                player_latitude=point[0],
+                                                player_longitude=point[1],
                                                 gym_latitude=fort['latitude'],
                                                 gym_longitude=fort['longitude']
                                                 )
